3f2c89546f317d8bd7616769feaa9240618d8195
Le Guillant Julien <julien.leguillant@orange.com>
2019-04-01 15:16:11 +0200

    update the method visibility

--- src/channel/channel.ts
+++ src/channel/channel.ts
@@ -65,23 +65,10 @@ export class Channel {
     }
 
     /**
-     * send Reply Command
-     * @param {number} id - Request identifier
-     * @param {string} dst - Destination identifier
-     * @param {any} data - Data of the reply
-     */
-    protected sendReply(id: number, dst: string, data: any) {
-        const message = new Transport(UUID, dst, EnumTransport.REPLY, id, new TransportMessage(this.name, data));
-        message.setStatus("OK");
-        Log.debug(TAG + "sendReply : " + JSON.stringify(message));
-        this.sendMessage(message);
-    }
-
-    /**
      * sendEvent
      * @param {any} data - Data of the event
      */
-    protected sendEvent(data: any) {
+    public sendEvent(data: any) {
         const message = new Transport(UUID, WILDCARD, EnumTransport.EVENT, Channel.sequenceMessage++,
             new TransportMessage(this.name, data));
         Log.debug(TAG + "sendEvent : " + JSON.stringify(message));
@@ -93,7 +80,7 @@ export class Channel {
      * @param {string} dst - Destination identifier
      * @param {any} data - Data of the command
      */
-    protected sendCommand(dst: string, data: any) {
+    public sendCommand(dst: string, data: any) {
         const message = new Transport(UUID, dst, EnumTransport.COMMAND, Channel.sequenceMessage++,
             new TransportMessage(this.name, data));
         Log.debug(TAG + "sendCommand : " + JSON.stringify(message));
@@ -101,6 +88,19 @@ export class Channel {
             this.waitingReplies[message.id] = resolve;
             this.sendMessage(message);
         });
+    }
+
+    /**
+     * send Reply Command
+     * @param {number} id - Request identifier
+     * @param {string} dst - Destination identifier
+     * @param {any} data - Data of the reply
+     */
+    protected sendReply(id: number, dst: string, data: any) {
+        const message = new Transport(UUID, dst, EnumTransport.REPLY, id, new TransportMessage(this.name, data));
+        message.setStatus("OK");
+        Log.debug(TAG + "sendReply : " + JSON.stringify(message));
+        this.sendMessage(message);
     }
 
     /**
